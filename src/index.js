98a49fe35ba054015b2d77924ef5ad321cf4fb72
jennyjean <jennyjean8675309@yahoo.com>
2019-04-04 17:03:27 -0400

    used middleware to call a function on every request made from client

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ let app = express()
 //connects us to the route we defined in user.js
 let userRoute = require('./routes/user')
 
+//using middleware to print every request that comes in (next is a reference to the next function in the pipeline)
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.originalUrl}`)
+
+  next()
+  //have to call next() when it's done or it will break the chain of functions
+})
 //telling express to register the route above
 app.use(userRoute)
 //serves static content to your app
