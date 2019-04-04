6808a834e9c64101f2f43d59749dba3648e516a8
jennyjean <jennyjean8675309@yahoo.com>
2019-04-04 16:10:30 -0400

    installed express, created an app with express in index.js, and a port, created a public folder and an index.html page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+//calls on the express library
+let express = require('express')
+
+//creates a new express app
+let app = express()
+
+//serves static content to your app
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.info(`App has started on port ${PORT}`))
