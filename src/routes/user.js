b797bb0b297209fd2033f4efd33377fd958e261a
jennyjean <jennyjean8675309@yahoo.com>
2019-04-04 16:53:31 -0400

    added a route that uses a query string to user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,20 @@
 let express = require('express')
 let router = express.Router()
 
+//using QueryString (a query property on the request object)
+//localhost:3000/user?username=jennyjean
 router.get('/user', (req, res) => {
+  if (req.query.username) {
+    res.send(`You have requested a user: ${req.query.username}`)
+  } else {
   res.send('You have requested a user')
+  }
+})
+
+//using the params property on the request object
+//localhost:3000/user/jennyjean
+router.get('/user/:username', (req, res) => {
+  res.send(`You have requested a user: ${req.params.username}`)
 })
 
 
