954249f75f0fe8e1393014d7289822074e681589
jennyjean <jennyjean8675309@yahoo.com>
2019-04-10 17:03:22 -0400

    created seed data

---
+++ seeds/users.js
@@ -0,0 +1,103 @@
+//Ready to seed database
+
+[
+  {
+    "cabinetName": "Jenny's Stuff",
+    "cabinetImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/17th-Century_Cabinet_of_Curiosities.jpg/800px-17th-Century_Cabinet_of_Curiosities.jpg",
+    "firstName": "Jenny",
+    "lastName": "Ingram",
+    "email": "jenny@gmail.com",
+    "username": "jennyjean",
+    "password": "password",
+    "public": true,
+    "items": [
+      {
+        "name": "La Sagrada Familia",
+        "description": "an unfinished cathedral by Antonio Gaudi",
+        "interpretation": "Gaudi's organic forms not only mimic nature, but also find God in nature.",
+        "location": "Barcelona, Spain",
+        "itemImage": "https://aws-tiqets-cdn.imgix.net/images/content/3ca6b020234e47448d46547ff3ac6b3f.jpg?auto=format&fit=crop&ixlib=python-1.1.2&q=25&s=434230a6bbd198e7be30fa3c234e13bd&w=400&h=320&dpr=2.625",
+        "itemType": "Artificialia"
+      },
+      {
+        "name": "Saturn V Rocket",
+        "description": "the rocket that took America's Apollo astronauts to the moon",
+        "interpretation": "The Saturn V is an amazing example of what human beings can achieve when they are highly motivated, and work together.",
+        "location": "",
+        "itemImage": "https://cdn.mos.cms.futurecdn.net/QpnB8NFxvo7ktcY4t5DCij-320-80.jpg",
+        "itemType": "Scientifica"
+      }
+    ]
+  },
+
+  {
+    "cabinetName": "Lucy's Cabinet",
+    "cabinetImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Berlin_Naturkundemuseum_Korallen.jpg/800px-Berlin_Naturkundemuseum_Korallen.jpg",
+    "firstName": "Lucy",
+    "lastName": "Ingram",
+    "email": "lucy@gmail.com",
+    "username": "lucyGoosey",
+    "password": "password",
+    "public": true,
+    "items": [
+      {
+        "name": "toe beans",
+        "description": "kitten toe beans",
+        "interpretation": "Toe beans make me so happy I could cry.",
+        "location": "",
+        "itemImage": "https://pics.me.me/thebestoftumbling-toe-beans-2858941.png",
+        "itemType": "Naturalia"
+      },
+      {
+        "name": "Unicorn Horn",
+        "description": "a unicorn's horn, or maybe... a narwhal tusk",
+        "interpretation": "One tiny example of how living in the Middle Ages *might* have fun.",
+        "location": "Brown University Library, Providence, Rhode Island",
+        "itemImage": "https://library.brown.edu/create/unicornfound/wp-content/uploads/sites/3/2015/02/narwhaltusk-300x237.jpg",
+        "itemType": "Exotica"
+      }
+    ]
+  },
+
+  {
+    "cabinetName": "Sterling's Collection",
+    "cabinetImage": "https://cb2.scene7.com/is/image/CB2/AlbaWardrobeSHF18_1x1/$web_setitem326$/180522110834/alba-white-lacquer-wardrobe.jpg",
+    "firstName": "Sterling",
+    "lastName": "Ingram",
+    "email": "sterling@gmail.com",
+    "username": "sterfry",
+    "password": "password",
+    "public": false,
+    "items": [
+      {
+        "name": "The Appalachian Trail",
+        "description": "a 2,200 mile hiking trail between Georgia and Maine in the US",
+        "interpretation": "Peace and calm.",
+        "location": "The US",
+        "itemImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/AT_-_Franconia_Ridge.JPG/300px-AT_-_Franconia_Ridge.JPG",
+        "itemType": "Naturalia"
+      }
+    ]
+  },
+
+  {
+    "cabinetName": "Oscar's Stuff",
+    "cabinetImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Musei_Wormiani_Historia.jpg/1024px-Musei_Wormiani_Historia.jpg",
+    "firstName": "Oscar",
+    "lastName": "Ingram",
+    "email": "ozman@gmail.com",
+    "username": "OzManTheGreat",
+    "password": "password",
+    "public": true,
+    "items": [
+      {
+        "name": "The Royal Game of Ur",
+        "description": "a two-player strategy race board game from ancient Mesopotamia",
+        "interpretation": "Over 4,000 years ago, people were looking for ways to be competitive and have fun.",
+        "location": "British Museum, London, UK",
+        "itemImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/British_Museum_Royal_Game_of_Ur.jpg/1024px-British_Museum_Royal_Game_of_Ur.jpg",
+        "itemType": "Artificialia"
+      }
+    ]
+  }
+]
